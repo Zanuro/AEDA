2a4a8d35f10489bc45862ca17d8cf844b1ae34da
Zanuro <vladcomanescu@yahoo.com>
2018-04-17 00:48:30 +0100

    Update main.cpp

--- p5/main.cpp
+++ p5/main.cpp
@@ -10,13 +10,21 @@
 using namespace std;
 
 
-
 void randomizar(vector<dni> &v){
 
     for (int i=0;i<v.size();i++)
         v[i].set_random();
 }
 
+
+void calcular_comp(vector<int> &comp, vector<vector<float>> &v, float suma, unsigned int algoritmo){
+
+        heap_sort<int>(comp, comp.size(), true);
+
+        v[algoritmo][0] = comp.front();
+        v[algoritmo][1] = (suma/comp.size());
+        v[algoritmo][2] = comp.back();
+}
 
 
 int main(){
@@ -26,15 +34,14 @@ int main(){
     int algortimo_ordenacion = 100;
     int npruebas=0;
 
-
-    while((opcion!=1)&&(opcion!=2)){
+do{
         cout << "Modos posibles en los que se puede trabajar " << endl;
         cout << "1.Modo demonstracion" << endl;
         cout << "2.Modo estadistica" << endl;
         cout << "Opcion?" << endl;
-        cin >> opcion;
-    }
+        cin >> opcion;}
 
+while((opcion!=1)&&(opcion!=2));
 
     if(opcion == 1){
 
@@ -44,6 +51,10 @@ int main(){
 
         cout << "Introduzca el tamano de la secuencia(por defecto es 25): " << endl;
         cin >> size_prueba;
+        if(size_prueba <= 0){
+            size_prueba=25;
+        }
+
         cout << "Algoritmos posibles para la ordenacion " << endl;
         cout << "1.Algoritmo BubbleSort" << endl;
         cout << "2.Algoritmo InsertionSort" << endl;
@@ -53,7 +64,6 @@ int main(){
         cout << "Opcion?" << endl;
         cin >> algortimo_ordenacion;
 
-                
     }
 
     vector<dni> vec(size_prueba);
@@ -91,37 +101,37 @@ int main(){
         cin >> size_prueba;
         cout << "Introduzca el numero de pruebas que se quiere hacer: " << endl;
         cin >> npruebas;
-        cout << "---------Número de Comparaciones-----------" << endl;
-        cout << "-------Mínimo--------Medio-------Máximo-------"  << endl;
+        cout << "------------Número de Comparaciones-----------" << endl;
+        cout << "------Mínimo----------Medio--------Máximo-------"  << endl;
 
 
         vector<dni> vector_prueba(size_prueba);
         vector<vector<float>> resultados(5,vector<float>(3));
-        vector<float> comparacion(npruebas);
+        vector<int> comparacion(npruebas);
         float suma_total=0;
+        int v=0;
 
-        for(int z=0;z<5;z++){
+        while(v<5){
 
-            switch (z) {
-            case 0:{
+          switch (v){
+
+            case 0:
 
             for(int i=0;i<npruebas;i++){
 
                 randomizar(vector_prueba);
-                comparacion[i]=bubble_sort<dni>(vector_prueba,vector_prueba.size(),true);
+                comparacion[i]=bubble_sort<dni>(vector_prueba,size_prueba,true);
                 suma_total+=comparacion[i];
                                             }
-                insertion_sort<float>(comparacion,size_prueba,true);
-                resultados[0][0] = comparacion.front();
-                resultados[0][1] = (suma_total/comparacion.size());
-                resultados[0][2] = comparacion.back();
+                calcular_comp(comparacion, resultados, suma_total, 0);
 
                 cout << "BubbleSort: " << setw(10) << resultados[0][0] << setw(10) << resultados[0][1] << setw(10) << resultados[0][2] << endl;
                 suma_total=0;
-                break;}
+                v++;
+             break;
 
 
-            case 1:{
+            case 1:
 
 
             for(int i=0;i<npruebas;i++){
@@ -131,15 +141,14 @@ int main(){
                  suma_total+=comparacion[i];
                                             }
 
-                insertion_sort<float>(comparacion,size_prueba,true);
-                resultados[1][0] = comparacion.front();
-                resultados[1][1] = (suma_total/comparacion.size());
-                resultados[1][2] = comparacion.back();
+                calcular_comp(comparacion, resultados, suma_total, 1);
 
                 cout << "InsertionSort: " << setw(10) << resultados[1][0] << setw(10) << resultados[1][1] << setw(10) << resultados[1][2] << endl;
-                suma_total=0;}
+                suma_total=0;
+                v++;
+              break;
 
-           case 2:{
+           case 2:
 
            for(int i=0;i<npruebas;i++){
 
@@ -147,49 +156,45 @@ int main(){
                 comparacion[i]=heap_sort<dni>(vector_prueba,size_prueba,true);
                 suma_total+=comparacion[i];
                                             }
-                insertion_sort<float>(comparacion,size_prueba,true);
-                resultados[2][0] = comparacion.front();
-                resultados[2][1] = (suma_total/comparacion.size());
-                resultados[2][2] = comparacion.back();
+                calcular_comp(comparacion, resultados, suma_total, 2);
 
                 cout << "HeapSort: " << setw(10) << resultados[2][0] << setw(10) << resultados[2][1] << setw(10) << resultados[2][2] << endl;
-                suma_total=0;}
+                suma_total=0;
+                v++;
+              break;
 
-            case 3:{
+            case 3:
 
-         for(int i=0;i<npruebas;i++){
+            for(int i=0;i<npruebas;i++){
 
                 randomizar(vector_prueba);
                 comparacion[i]=quicksort<dni>(vector_prueba,size_prueba,true);
                 suma_total+=comparacion[i];
                                          }
-                insertion_sort<float>(comparacion,size_prueba,true);
-                resultados[3][0] = comparacion.front();
-                resultados[3][1] = (suma_total/comparacion.size());
-                resultados[3][2] = comparacion.back();
+                calcular_comp(comparacion, resultados, suma_total, 3);
 
                 cout << "QuickSort: " << setw(10) << resultados[3][0] << setw(10) << resultados[3][1] << setw(10) << resultados[3][2] << endl;
-                suma_total=0;}
+                suma_total=0;
+                v++;
+              break;
 
-           case 4:{
 
-             for(int i=0;i<npruebas;i++){
+           case 4:
+
+           for(int i=0;i<npruebas;i++){
 
                 randomizar(vector_prueba);
                 comparacion[i]=shell_sort<dni>(vector_prueba,size_prueba,true);
                 suma_total+=comparacion[i];
                                              }
-                insertion_sort<float>(comparacion,size_prueba,true);
-                resultados[4][0] = comparacion.front();
-                resultados[4][1] = (suma_total/comparacion.size());
-                resultados[4][2] = comparacion.back();
+                calcular_comp(comparacion, resultados, suma_total, 4);
 
                 cout << "ShellSort: " << setw(10) << resultados[4][0] << setw(10) << resultados[4][1] << setw(10) << resultados[4][2] << endl;
-                suma_total=0;}
-
+                suma_total=0;
+                v++;
+              break;
         }
 
     }
-
 }
 }
